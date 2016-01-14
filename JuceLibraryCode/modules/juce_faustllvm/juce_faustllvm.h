edd2a456e870eedc901846731f423f10ff8ad556
Oli Larkin <olilarkin@googlemail.com>
2016-01-14 12:04:29 +0000

    update modules and jucer project for juce 4

--- JuceLibraryCode/modules/juce_faustllvm/juce_faustllvm.h
+++ JuceLibraryCode/modules/juce_faustllvm/juce_faustllvm.h
@@ -1,42 +1,12 @@
-/*
-  This file is part of the juce_faustllvm JUCE module
-  Written by Oliver Larkin
-  Contemporary Music Research Centre
-  Music Department, University of York
-  License: GPL
-  
-  based on Faustgen~ by GRAME
-  
-*/
+// This is an auto-generated file to redirect any included
+// module headers to the correct external folder.
 
-#include <modules/juce_core/juce_core.h>
-#include <modules/juce_audio_processors/juce_audio_processors.h>
-#include <modules/juce_gui_basics/juce_gui_basics.h>
-
-#define FAUSTFLOAT float
-
-#define DEFAULT_FAUST_DSP_SEARCHPATH "~/Library/Application Support/pMix2/FaustNodes"
-
-#define FAUSTGEN_VERSION "0.99b"
-
-#if JUCE_MAC || JUCE_LINUX
-  #define FAUST_DRAW_PATH "/var/tmp/"
-  #define SEPARATOR '/'
-#elif JUCE_WIN32
-  #define FAUST_DRAW_PATH "\\faustgen-resources\\"
-  #define SEPARATOR '\\'
+#if defined (JUCER_XCODE_MAC_F6D2F4CF)
+ #include "../../../../juce_faustllvm/juce_faustllvm.h"
+#elif defined (JUCER_LINUX_MAKE_7346DA2A)
+ #include "../../../../juce_faustllvm/juce_faustllvm.h"
+#elif defined (JUCER_VS2013_78A5020)
+ #include "../../../../../juce/juce_faustllvm/juce_faustllvm.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
 #endif
-
-#define LLVM_OPTIMIZATION 3
-#define DEFAULT_CODE "declare name \"Faust Effect\";\n\ngain = hslider(\"gain\", 1., 0., 1., 0.);\nprocess = _*(gain);"
-//#define DEFAULT_CODE "import(\"math.lib\"); \nimport(\"maxmsp.lib\"); \nimport(\"music.lib\"); \nimport(\"oscillator.lib\"); \nimport(\"reduce.lib\"); \nimport(\"filter.lib\"); \nimport(\"effect.lib\"); \n \nprocess=_,_;"
-
-#define LOG Logger::getCurrentLogger()->writeToLog
-
-#include "source/FaustAudioPluginInstance.h"
-#include "source/FaustAudioProcessorParameter.h"
-#include "source/FaustCodeTokenizer.h"
-#include "source/FaustPluginFormat.h"
-// #include "source/FaustDSPWatcher.h"
-// #include "source/FaustFileFilter.h"
-// #include "source/FaustGenFactory.h"
